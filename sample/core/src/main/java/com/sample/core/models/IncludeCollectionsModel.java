a7dda966be3843e9220d7b005ab3b307f7d51190
JHuckins <josiah.huckin5@gmail.com>
2021-06-25 22:23:58 -0400

    Update IncludeCollectionsModel.java

--- sample/core/src/main/java/com/sample/core/models/IncludeCollectionsModel.java
+++ sample/core/src/main/java/com/sample/core/models/IncludeCollectionsModel.java
@@ -37,24 +37,25 @@ public class IncludeCollectionsModel {
 	private String contentType;
 	
 	@Self
-	 private Resource resource;
+	private Resource resource;
 	
 	@Inject
 	@Source("sling-object")
 	private ResourceResolver resourceResolver;
-
-
+	
+	
 	//Determine the type of path suffix to use when obtaining JCR node properties.
 	private String getPathSuffix() {
 		 String suffix = "";
 		 if (contentType.equals("text-fragments")) {
-			 suffix = "/" + JcrConstants.JCR_CONTENT + "/data/master";
+			suffix = "/" + JcrConstants.JCR_CONTENT + "/data/master";
 		 }
 		 if (contentType.equals("svg-icons")) {
-			 suffix = "/" + JcrConstants.JCR_CONTENT + "/renditions/original/" + JcrConstants.JCR_CONTENT;
+			suffix = "/" + JcrConstants.JCR_CONTENT + "/renditions/original/" + JcrConstants.JCR_CONTENT;
 		 }
 		 return suffix;
 	}
+	
 	
 	//Convert the jcr:data property value to a string.
 	private String getJcrData(InputStream binStream) throws IOException {
@@ -66,12 +67,13 @@ public class IncludeCollectionsModel {
 		}
 		return bA.toString(StandardCharsets.UTF_8.name());
 	}
+		
 	
 	private void removeCollectionProperties(Node node) throws RepositoryException {
 		PropertyIterator propertiesIterator = node.getProperties();
         	while (propertiesIterator.hasNext()) {
             		String propertyName = propertiesIterator.nextProperty().getName();
-            		if (propertyName.contains("id")) {
+            		if (propertyName.contains("_")) {
             			node.getProperty(propertyName).remove();
             		}
         	}
@@ -81,51 +83,63 @@ public class IncludeCollectionsModel {
 		try
 		{		
 			Node node = null;
-			Node resourceNode = null;
-			Node currentNode = resource.adaptTo(Node.class);
+			Node nameNode = null;
+			Node resNode = null;
 			String pathSuffix = getPathSuffix();
+	    		ModifiableValueMap currentResource = resource.adaptTo(ModifiableValueMap.class);
+			Node currentNode = resource.adaptTo(Node.class);
 			
-		    if(resourcePath != null) {
-			    
-			    	//Remove the existing "id" properties, which will be re-added if they are listed in sling:resources.
+		   	 if(resourcePath != null) {		    	
+				//Clear out the existing id properties, which will be re-added if they are listed in sling:resources.
 		    		removeCollectionProperties(currentNode);
-			    
-				Resource res = resourceResolver.getResource(resourceParth + "/sling:members");
+		    		Resource res = resourceResolver.getResource(resourcePath + "/sling:members");
 				node = res.adaptTo(Node.class);
-				Property slingRes = node.getProperty("sling:resources");
-				
+		    		Property slingRes = node.getProperty("sling:resources");
 					if (slingRes != null) {
-					    	 Value[] paths = slingRes.getValues();					    	 
-					    	    for (int i=0; i<paths.length; i++) {
-					    	       Resource contentResource = resourceResolver.getResource(paths[i].toString() + pathSuffix);
-				    	    	       resourceNode = contentResource.adaptTo(Node.class); 
-					    	       if (contentType.equals("text-fragments")) {
-						           //Obtain the current fragment's property which contains the text content.
-					    	    	   String txtVal = resourceNode.getProperty("text-content").getValue().toString();					    	    	   
-					    	    	   currentNode.setProperty("id "+i,"value " + txtVal);					    	    	   
-					    	       } else if (contentType.equals("svg-icons" )) {
-								   //Get the SVG markup if the mimetype indicates the current node is an SVG image.
-					    	    		   String mimeType = resourceNode.getProperty(JcrConstants.JCR_MIMETYPE).getValue().toString();					    	    	   
-						    	    	   if (mimeType.equals("image/svg+xml")) {								    	    	   
-								    	    	   Property iconBin = resourceNode.getProperty(JcrConstants.JCR_DATA);
+						Value[] paths = slingRes.getValues();
+							for (int i=0; i<paths.length; i++) {
+					    	  		Resource nameRes = resourceResolver.getResource(paths[i].toString());
+					    	       		Resource contentRes = resourceResolver.getResource(paths[i].toString() + pathSuffix);
+					    	       		nameNode = nameRes.adaptTo(Node.class); 
+				    	    	   		resNode = contentRes.adaptTo(Node.class); 
+				    	    	   		String name = nameNode.getName(); 
+
+				    	    	   		//Remove the extension and period from the node name, to avoid property namespacing issues.
+				    	    	  		name = name.substring(0, name.length() - 4);
+				    	    	  		if (name.substring(name.length() - 1).equals(".")) {
+				    	    		   		name = name.substring(0, name.length() - 1);
+				    	    	   		}
+				    	    	   				    	    	   
+					    	       		if (contentType.equals("text-fragments")) {
+                                       					//Obtain the current fragment's property which contains the text content.
+					    	    	  		String txtVal = resNode.getProperty("text-content").getValue().toString();							    	 
+					    	    	  		currentResource.put("_"+name + " ",txtVal);					    	    	   
+					    	       		} else if (contentType.equals("svg-icons" )) {
+					    	    		   	String mimeType = resNode.getProperty(JcrConstants.JCR_MIMETYPE).getValue().toString();					    	    	   
+						    	    	   	//Get the SVG markup if the mimetype indicates the current node is an SVG image.
+                                           				if (mimeType.equals("image/svg+xml")) {								    	    	   
+								    	    	   Property iconBin = resNode.getProperty(JcrConstants.JCR_DATA);
 								    	           Binary binary =  iconBin.getBinary();
 								    	           InputStream binStream = binary.getStream();	
-								    	           String iconVal = getJcrData(binStream);							    	        
-								    	    	   currentNode.setProperty("id "+i,"value " + iconVal);
-						    	    	   }						    	    	
-					    	       } else {
-							   //If the current node isn't a content fragment or SVG, return it's resource path.
-					    	    	   currentNode.setProperty("id "+i,"resource " + paths[i].toString());
-					    	       }
-					    	    }
-					    	 currentNode.getSession().save();
+								    	           String iconVal = getDAMJcrData(binStream);							    	        
+								    	           currentResource.put("_"+name + " ",iconVal);
+						    	    		}						    	    	
+							       } else {
+                                       					//If the current node isn't a content fragment or SVG, return it's resource path.
+					    	    	   		currentResource.put("_"+name + " ",paths[i].toString());
+					    	       		}
+					    	       
+					    	    	}
+							
 					}
 				    
 			 }
+			
+		    resourceResolver.commit();  
+			
 		} catch(Exception e) {
-			log.error("{} Include Collections Model cannot generate data.", LOG_PREFIX);
+			log.error("{} Include Collections Model cannot obtain collection data.", LOG_PREFIX);
 			log.error(e.getMessage(), e);
 		}
 	}
-
 }
