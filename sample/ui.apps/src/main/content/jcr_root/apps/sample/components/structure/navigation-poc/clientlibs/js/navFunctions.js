0e48761da9fdc51782b2dbdf80126658ba9301b0
Josiah <josiah.huckins@perficient.com>
2024-04-22 10:57:35 -0400

    Adding vw resize handling for the menu

--- sample/ui.apps/src/main/content/jcr_root/apps/sample/components/structure/navigation-poc/clientlibs/js/navFunctions.js
+++ sample/ui.apps/src/main/content/jcr_root/apps/sample/components/structure/navigation-poc/clientlibs/js/navFunctions.js
@@ -1,6 +1,6 @@
-function toggleMobileNav() {
-    var menu = document.getElementById("site-menu");
-    
+var menu = document.getElementById("site-menu");
+
+function toggleMobileNav() {    
     if (menu.style.display === "none") {
       menu.style.display = "block";
     } else {
@@ -11,4 +11,13 @@ function toggleMobileNav() {
 window.addEventListener('load', function() {
     var menuButton = document.getElementById("mobile-nav");
     menuButton.addEventListener('click', toggleMobileNav); 
+});
+
+window.addEventListener('resize', function() {
+    var vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+    if (vw < 1000) {
+      menu.style.display = "none";
+    } else if (vw > 1000) {
+      menu.style.display = "block";
+    }
 });
